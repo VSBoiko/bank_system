0506210fca1b47ff4f68eb5f6bd8dc2f76594c27
vlad boiko <mail@vladboiko.ru>
2022-03-13 14:53:21 +0300

    add main file bank.py

--- bank.py
+++ bank.py
@@ -1,5 +1,14 @@
-import sys
 import os
+import pickle
+
+from settings import pickle_file
+from main import Bank
+
+if os.path.isfile(pickle_file):
+    os.remove(pickle_file)
+    bank = Bank()
+    with open(pickle_file, 'wb') as f:
+        pickle.dump(bank, f)
 
 dir_path = os.path.abspath(os.curdir)
 
